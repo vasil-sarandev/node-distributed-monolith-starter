1aac704de82b07ce60dda24a06a6ce4d1d54f952
Vasil Sarandev <vasil.sarandev@gmail.com>
2026-07-18 14:47:04 +0300

    add health check route

--- src/api/modules/app-router.ts
+++ src/api/modules/app-router.ts
@@ -5,6 +5,8 @@ import { userRouter } from '@api/modules/user/user.router';
 
 export const appRouter = Router();
 
+appRouter.get('/health', (_req, res) => res.sendStatus(200));
+
 appRouter.use('/api/user', userRouter);
 appRouter.use('/api/product', productRouter);
 appRouter.use('/api/event', eventHandlerRouter);
